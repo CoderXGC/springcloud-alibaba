8d25d6ad3d631763dae61dc9c9b506b40b70b77a
CoderXGC <xgc@tom.com>
2022-01-31 23:50:45 +0800

    init something

--- shop-order/src/main/java/com/ylesb/controller/OrderController.java
+++ shop-order/src/main/java/com/ylesb/controller/OrderController.java
@@ -8,22 +8,16 @@ package com.ylesb.controller;
  * @date 2022/1/2510:39
  */
 
-import com.alibaba.fastjson.JSON;
 import com.ylesb.domain.Order;
 import com.ylesb.domain.Product;
 import com.ylesb.service.OrderService;
 import com.ylesb.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Random;
 
 /**
  * @className    : OrderController
